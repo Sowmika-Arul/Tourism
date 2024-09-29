8bf825cc2f92b0163a1292b9cda3c4b547c21239
Sowmika-Arul <sowmikaarulkumar@gmail.com>
2024-09-29 23:27:29 +0530

    places added

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session
-from flask_login import current_user
+from flask import Flask, render_template
 import mysql.connector
 import secrets
 
@@ -19,11 +18,26 @@ config = {
 def get_db_connection():
     return mysql.connector.connect(**config)
 
-# Route to display login form
+# Fetch travel package data from the database
+def get_travel_packages():
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    query = "SELECT destination_name, image_url, highlights, total_days, amount FROM travel_packages"
+    cursor.execute(query)
+    packages = cursor.fetchall()  # Fetch all rows from travel_packages table
+    cursor.close()
+    connection.close()
+    return packages
+
 @app.route('/')
-def login():
+def index():
     return render_template('index.html')
 
+# Route to display travel packages
+@app.route('/places')
+def show_travel_packages():
+    travel_packages = get_travel_packages()  # Fetch data from the DB
+    return render_template('places.html', packages=travel_packages)  # Pass data to the template
 
 if __name__ == '__main__':
     app.run(debug=True)
